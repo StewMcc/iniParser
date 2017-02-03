d9245af714eb8d779d4405de3c2348b006eb21ff
Stewart L. McCready <stewmcc@gmail.com>
2017-02-03 22:23:22 +0000

    Updated for my needs, VS 2015.  - stable mlog version (old uncompilable)  - removed warnings ( strcopy_s,sprintf_s,size_t,stof/atof)  - removed new/delete in test.  - configdebug to #ifdef instead of if().  - tryed to reduce #include contamination

--- test.cpp
+++ test.cpp
@@ -1,26 +1,24 @@
 #include "ConfigINI.h"
 #include <iostream>
 
-int main()
-{
-    ConfigINI *ini = new ConfigINI("test.ini");
-    ini->setIntValue("section1", "intValue", 1);
-    ini->setFloatValue("section1", "floatValue", 0.1);
-    ini->setStringValue("section2", "stringValue", "hello, world");
-    ini->setBoolValue("section2", "boolValue", true);
-    ini->writeConfigFile();
+int main() {
+	ConfigINI ini = ConfigINI("test.ini");
+	ini.setIntValue("section1", "intValue", 1);
+	ini.setFloatValue("section1", "floatValue", 0.1f);
+	ini.setStringValue("section2", "stringValue", "hello, world");
+	ini.setBoolValue("section2", "boolValue", true);
+	ini.writeConfigFile();
 
-    int intValue = ini->getIntValue("section1", "intValue");
-    float floatValue = ini->getFloatValue("section1", "floatValue");
-    const char *stringValue = ini->getStringValue("section2", "stringValue");
-    bool boolValue = ini->getBoolValue("section2", "boolValue");
+	int intValue = ini.getIntValue("section1", "intValue");
+	float floatValue = ini.getFloatValue("section1", "floatValue");
+	const char *stringValue = ini.getStringValue("section2", "stringValue");
+	bool boolValue = ini.getBoolValue("section2", "boolValue");
 
-    cout<<"[section1]-intValue:\t"<<intValue<<endl;
-    cout<<"[section1]-floatValue:\t"<<floatValue<<endl;
-    cout<<"[section2]-stringValue:\t"<<stringValue<<endl;
-    cout<<"[section2]-boolValue:\t"<<boolValue<<endl;
+	std::cout << "[section1]-intValue:\t" << intValue << std::endl;
+	std::cout << "[section1]-floatValue:\t" << floatValue << std::endl;
+	std::cout << "[section2]-stringValue:\t" << stringValue << std::endl;
+	std::cout << "[section2]-boolValue:\t" << boolValue << std::endl;
 
-    delete ini;
-    return 0;
+	return 0;
 }
 
